10e70a0387578aeca8ff8055b557ff8ed829ae28
Teng Ma (马腾) <teng-ma@linux.alibaba.com>
2024-10-12 15:46:54 +0800

    [FEATURE] add CXL transport
    Based on CXL, We introduce a new transport. It can use shared memory to transfer data.

---
+++ mooncake-transfer-engine/src/transport/cxl_transport/cxl_transport.cpp
@@ -0,0 +1,64 @@
+#include "transport/cxl_transport/cxl_transport.h"
+#include "common.h"
+#include "transfer_engine.h"
+#include "transfer_metadata.h"
+#include "transport/transport.h"
+#include <algorithm>
+#include <bits/stdint-uintn.h>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <glog/logging.h>
+#include <iomanip>
+#include <memory>
+
+namespace mooncake
+{
+    CxlTransport::CxlTransport()
+    {
+        //TODO
+    }
+
+    CxlTransport::~CxlTransport() {}
+    
+    CxlTransport::BatchID CxlTransport::allocateBatchID(size_t batch_size)
+    {
+        auto batch_id = Transport::allocateBatchID(batch_size);
+        return batch_id;
+    }
+
+    int CxlTransport::getTransferStatus(BatchID batch_id, size_t task_id, TransferStatus &status)
+    {
+        return 0;
+    }
+
+    int CxlTransport::submitTransfer(BatchID batch_id, const std::vector<TransferRequest> &entries)
+    {
+        return 0;
+    }
+
+    int CxlTransport::freeBatchID(BatchID batch_id)
+    {
+        return 0;
+    }
+
+    int CxlTransport::install(std::string &local_server_name, std::shared_ptr<TransferMetadata> meta, void **args)
+    {
+        return 0;
+    }
+
+    int CxlTransport::registerLocalMemory(void *addr, size_t length, const string &location, bool remote_accessible, bool update_metadata)
+    {
+        return 0;
+    }
+
+    int CxlTransport::unregisterLocalMemory(void *addr, bool update_metadata)
+    {
+        return 0;
+    }
+
+    void CxlTransport::addSliceToTask(void *source_addr, uint64_t slice_len, uint64_t target_start, TransferRequest::OpCode op, TransferTask &task, const char *file_path)
+    {
+        // TODO
+    }
+}
